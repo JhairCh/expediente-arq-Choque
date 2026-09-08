a683ab5f8fb0c5bd4ab48f43b9dd6011bd1368e8
Jhair Anderson Choque <Jhair69tesla@gmail.com>
2026-09-07 20:33:32 -0400

    Avance de refactor Despues

--- Parcial1/refactor.cs
+++ Parcial1/refactor.cs
@@ -1,4 +1,5 @@
 //Cura I:
+namespace Parcial1.Farmacia.Antes;
 
 public interface IEmpleadoDeFarmacia
 {
@@ -24,4 +25,45 @@ public class Cajero : IEmpleadoDeFarmacia
     public void VerLibroDeControlados()
         => throw new NotSupportedException();
 }
+
+//este es el Empleado de Farmaceutica:
+public class Farmaceutico : IEmpleadoDeFarmacia
+{
+    public void RegistrarPedido(string medicamento, int cantidad)
+        => Console.WriteLine($"[FARM] Pedido: {cantidad} x {medicamento}");
+
+    public void AutorizarVentaControlada(string medicamento)
+        => Console.WriteLine($"[FARM] Venta controlada de {medicamento} autorizada");
+
+    public void AjustarPrecio(string medicamento, decimal nuevoPrecio)
+        => Console.WriteLine($"[FARM] {medicamento} ahora cuesta {nuevoPrecio:0.00} Bs");
+
+    public void VerLibroDeControlados()
+        => Console.WriteLine("[FARM] Libro de medicamentos controlados");
+}
+
+
+//Despues
+//Refactor : Jhair Anderson Choque 
+
+public interface IRegistradorDePedidos
+{
+    void RegistrarPedido(string medicamento, int cantidad);
+}
+
+public interface IAutorizadorDeVentaControlada
+{
+    void AutorizarVentaControlada(string medicamento);
+}
+
+public interface IAjustadorDePrecios
+{
+    void AjustarPrecio(string medicamento, decimal nuevoPrecio);
+}
+
+public interface ILectorLibroDeControlados
+{
+    void VerLibroDeControlados();
+}
+
 
