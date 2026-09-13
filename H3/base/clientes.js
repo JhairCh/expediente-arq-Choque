4fc47e465f1ec09338cc93ec2091c06b9c4ff0e6
Jhair Anderson Choque <Jhair69tesla@gmail.com>
2026-09-13 17:18:30 -0400

    Avance de H3

---
+++ H3/base/clientes.js
@@ -0,0 +1,8 @@
+class Cliente {
+    constructor(id, nombre, telefono, email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+}
