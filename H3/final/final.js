0c411fbb0c6a135cddb8e2bfe09c08a112a004df
Jhair Anderson Choque <Jhair69tesla@gmail.com>
2026-09-20 16:53:59 -0400

    Final Fusion

--- H3/final/final.js
+++ H3/final/final.js
@@ -23,5 +23,70 @@ class AsignarPorDisponibilidad extends EstrategiaAsignacion {
     }
 }
 
+//Observer
+
+class Cliente {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+
+    actualizar(orden) {
+        console.log(
+            `Cliente ${this.nombre} fue notificado: ` +
+            `la orden ${orden.id} está ${orden.estado}.`
+        );
+    }
+}
+
+class Tecnico {
+    constructor(nombre, especialidad, disponibilidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.disponibilidad = disponibilidad;
+    }
+
+    actualizar(orden) {
+        console.log(
+            `Tecnico ${this.nombre} fue notificado: ` +
+            `la orden ${orden.id} esta ${orden.estado}.`
+        );
+    }
+}
 
 
+// Orden de Trabajo
+
+class OrdenDeTrabajo {
+    constructor(id, especialidad, estrategia) {
+        this.id = id;
+        this.especialidad = especialidad;
+        this.estado = "RECIBIDA";
+        this.tecnicoAsignado = null;
+
+        this.estrategia = estrategia;
+
+        this.observadores = [];
+    }
+
+    suscribir(observador) {
+        this.observadores.push(observador);
+    }
+
+    asignarTecnico(tecnicos) {
+        this.tecnicoAsignado = this.estrategia.asignar(
+            this,
+            tecnicos
+        );
+
+        if (this.tecnicoAsignado) {
+            console.log(
+                `Tecnico asignado: ${this.tecnicoAsignado.nombre}`
+            );
+
+            this.cambiarEstado("EN_REPARACION");
+        } else {
+            console.log("No se encontro un tecnico disponible.");
+        }
+    }
+
+}
